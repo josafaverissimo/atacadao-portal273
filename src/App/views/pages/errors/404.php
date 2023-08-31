4df4f2b75f35c96819a81d950500cf82dd6a0c7c
josafa <josafaverissimo98@gmail.com>
2023-08-31 03:06:41 +0000

    online changes

--- src/App/views/pages/errors/404.php
+++ src/App/views/pages/errors/404.php
@@ -7,16 +7,6 @@
 <?php $this->endSection("head"); ?>
 
 <main class="container bg-white rounded-3 border shadow-lg">
-    <div class="row pt-3">
-        <div class="d-flex justify-content-center">
-            <img alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/gokukid.gif"); ?>">
-            <img class="strong-tilt-move-shake" alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/error404.png"); ?>">
-            <img alt="mk" class="flip-image" src="<?= Helpers::baseUrl("/assets/imgs/gokukid.gif"); ?>">
-        </div>
-        <div class="d-flex justify-content-center">
-            <img alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/shaokhanfinishhim.gif"); ?>">
-        </div>
-    </div>
     <div class="row">
         <div class="p-3 p-lg-5 pt-lg-3 d-flex justify-content-evenly">
             <div>
@@ -38,6 +28,16 @@
             <div>
                 <img alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/liukang-dancando.gif"); ?>">
             </div>
+        </div>
+    </div>
+    <div class="row pt-3">
+        <div class="d-flex justify-content-center">
+            <img alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/gokukid.gif"); ?>">
+            <img class="strong-tilt-move-shake" alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/error404.png"); ?>">
+            <img alt="mk" class="flip-image" src="<?= Helpers::baseUrl("/assets/imgs/gokukid.gif"); ?>">
+        </div>
+        <div class="d-flex justify-content-center">
+            <img alt="mk" src="<?= Helpers::baseUrl("/assets/imgs/shaokhanfinishhim.gif"); ?>">
         </div>
     </div>
 </main>
