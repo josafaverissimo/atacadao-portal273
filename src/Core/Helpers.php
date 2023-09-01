21000a7131fcad0f34ce93fb1abb8fd69e64bab9
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-01 04:22:49 -0300

    refactor(Dataset): functions getData and getJsonFileData are now in Helpers

--- src/Core/Helpers.php
+++ src/Core/Helpers.php
@@ -70,4 +70,22 @@ final class Helpers
         }
         echo "</script>";
     }
+
+    public static function getData(string $file): array|object
+    {
+        return require Helpers::baseDatasetPath($file) . ".php";
+    }
+
+    public static function getJsonFileData(string $jsonFile): array
+    {
+        $filepath = Helpers::baseDatasetPath("/json/{$jsonFile}") . ".json";
+        $jsonFileStream = fopen($filepath, "rb");
+        $jsonData = "";
+
+        while(!feof($jsonFileStream)) {
+            $jsonData .= fread($jsonFileStream, 4096);
+        }
+
+        return json_decode($jsonData);
+    }
 }
