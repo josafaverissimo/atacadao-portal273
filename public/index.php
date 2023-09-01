505fc7fe88ef41bd250fcf254e282c18aeb31668
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-01 04:25:27 -0300

    feat(index): new route added

--- public/index.php
+++ public/index.php
@@ -9,8 +9,13 @@ $routes = new Router();
 $routes->get("/", CONF_DEFAULT_CONTROLLER);
 $routes->get("/link-center", "LinkCenter:index");
 $routes->get("/clock", "Clock:index");
+$routes->group(["prefix" => "phones"], function() {
+    $this->get("/", "Phones:index");
+    $this->get("/unitPhones/unitId/(:any)", "Phones:getUnitPhonesByUnitId");
+});
 $routes->get("/phones", "Phones:index");
 $routes->get("/reports", CONF_CONTROLLER_BUILDING_PAGE);
 $routes->get("/best-practices", CONF_CONTROLLER_BUILDING_PAGE);
+
 
 $routes->dispatch();
