a2261c519eabbb3ac3e787d9fed10014eb5d574a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-01 04:27:47 -0300

    feat(phones): new page created

--- src/App/Controllers/Phones.php
+++ src/App/Controllers/Phones.php
@@ -2,27 +2,62 @@
 
 namespace Src\App\Controllers;
 
+use Src\App\Dataset\UnitsPhones;
 use Src\Core\Controller;
-use Src\App\Dataset;
+use Src\Core\Helpers;
 
 class Phones extends Controller
 {
     public function index(): void
     {
-        $phones = [
-            ...Dataset::getJsonFileData("main_unit"),
-            ...Dataset::getJsonFileData("random_unit"),
-            ...Dataset::getJsonFileData("phones_unit")
-        ];
-        $units = Dataset::getJsonFileData("units");
+        $unitId = "273";
+        $unitsOptions = array_reduce(
+            Helpers::getJsonFileData("units"),
+            function (array $options, object $unit) use ($unitId) {
+                $unitFiltered = new \StdClass();
+                $unitFiltered->id = $unit->id_filial;
+                $unitFiltered->value = $unit->id_filial;
+                $unitFiltered->selected = str_contains($unit->id_filial, $unitId);
+                $unitFiltered->textContent = mb_convert_case(
+                    str_replace("-", " ", $unit->descricao), MB_CASE_TITLE
+                );
 
+                return [...$options, $unitFiltered];
+            }, []
+        );
+        $unitPhonesRows = $this->getUnitPhonesDataByUnitId($unitId);
 
         $data = [
             "title" => "Lista de Ramais",
-            "phones" => $phones,
-            "units" => $units
+            "unitPhonesRows" => $unitPhonesRows,
+            "unitsOptions" => $unitsOptions
         ];
 
         $this->renderView("/pages/phones/index", $data);
+    }
+
+    private function getUnitPhonesDataByUnitId($unitId) {
+        $unitPhonesDataset = new UnitsPhones();
+        return array_reduce(
+            $unitPhonesDataset->getUnitPhonesByUnitId($unitId),
+            function(array $rows, object $unitPhones) {
+                $row = [
+                    $unitPhones->phone,
+                    mb_convert_case($unitPhones->owner, MB_CASE_TITLE),
+                    mb_convert_case($unitPhones->sector, MB_CASE_TITLE)
+                ];
+
+                return [...$rows, $row];
+            }, []
+        );
+    }
+
+    public function getUnitPhonesByUnitId($unitId): void
+    {
+        header("Content-type: application/json");
+        $unitPhonesRows = $this->getUnitPhonesDataByUnitId($unitId);
+        echo json_encode([
+            "rows" => $unitPhonesRows
+        ]);
     }
 }
--- src/App/views/pages/phones/index.php
+++ src/App/views/pages/phones/index.php
@@ -2,54 +2,38 @@
 
 <?php $this->template("base", ["title" => $title]); ?>
 
-<?php $this->setSection("head"); ?>
-    <link rel="stylesheet" href="<?= Helpers::baseUrl("/assets/css/phones/styles.css"); ?>">
-<?php $this->endSection("head"); ?>
-
 <main class="container-fluid container-lg">
     <div class="p-4 pb-0 p-lg-0 pt-lg-5 p-md-5 align-items-center rounded-3 border shadow-lg mb-3 bg-white">
         <div class="p-3 p-lg-5 pt-lg-3">
             <h1 class="display-5 fst-italic mb-3">Lista de Ramais</h1>
-            <div id="my-select" class="dropdown mb-2 my-select">
-                <select name="unit">
-                    <?php foreach($units as $unit): ?>
-                        <option id="my-select-option-<?= $unit->id_filial; ?>" value="<?= $unit->id_filial; ?>">
-                            <?= str_replace("-", " ", $unit->descricao); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <button class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" type="button">
-                    Selecione uma filial
-                </button>
 
-                <div class="dropdown-menu">
-                <ul class="list-group-flush px-2">
-                    <li class="list-group-item">
-                        <input type="text" class="form-control" placeholder="Filial">
-                        <div class="dropdown-divider"></div>
-                    </li>
-
-                    <ul class="p-0"></ul>
-                </ul>
+            <div class="d-flex justify-content-between mb-2">
+                <?=
+                    $this->getViewHtml("/components/my-select", [
+                        "options" => $unitsOptions,
+                        "buttonPlaceholder" => "Selecione a filial",
+                        "inputPlaceholder" => "Pesquise a filial",
+                        "id" => "select-unit",
+                        "name" => "unit",
+                    ]);
+                ?>
+                <div>
+                    <form id="phones-search-form" class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Digite algo para pesquisar" aria-label="Pesquisar">
+                        <button class="btn btn-outline-secondary" type="button">Limpar</button>
+                    </form>
                 </div>
             </div>
 
-            <table class="table table-striped table-bordered w-100">
-                <thead>
-                    <tr>
-                        <th>Número</th>
-                        <th>Setor</th>
-                        <th>Responsável</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>11111111111</td>
-                        <td>12313122133</td>
-                        <td>12312312323</td>
-                    </tr>
-                </tbody>
-            </table>
+            <?=
+                $this->getViewHtml("/components/my-table", [
+                    "id" => "unit-phones",
+                    "classes" => "",
+                    "thead" => ["Número", "Setor", "Responsável"],
+                    "rows" => $unitPhonesRows
+                ]);
+            ?>
+
         </div>
     </div>
 </main>
