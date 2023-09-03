750056db456a09aec91242383f8a4b8dfafd8c7b
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-03 17:26:14 -0300

    feat(Core\Html): a class Html was added to work with dom

---
+++ src/Core/Html.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Src\Core;
+
+class Html
+{
+    private \DOMDocument $dom;
+    private \DOMXPath $xpath;
+
+    public function __construct(string $html)
+    {
+        $this->dom = new \DOMDocument();
+        @$this->dom->loadHTML($html);
+        $this->xpath = new \DOMXPath($this->dom);
+    }
+
+    public function query(string $xpath): \DOMNodeList
+    {
+        return $this->xpath->query($xpath);
+    }
+}
