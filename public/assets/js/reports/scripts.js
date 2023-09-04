f68630af4eddc939e589304afdd6cd8084763dad
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-04 11:45:29 -0300

    feat(pages/reports): input to filter created

---
+++ public/assets/js/reports/scripts.js
@@ -0,0 +1,150 @@
+function ReportsListItem() {
+    function create(name, description, time, footer) {
+        const itemAnchor = document.createElement("a")
+        itemAnchor.href = "#"
+        itemAnchor.classList.add("list-group-item", "list-group-item-action", "flex-column", "align-items-start")
+
+        const itemWrapperDiv = document.createElement("div")
+        itemWrapperDiv.classList.add("d-flex", "w-100", "justify-content-between")
+        itemAnchor.appendChild(itemWrapperDiv)
+
+        const itemReportNameH1 = document.createElement("h1")
+        itemReportNameH1.classList.add("h5", "mb-1")
+        itemReportNameH1.textContent = name
+        itemWrapperDiv.appendChild(itemReportNameH1)
+
+        const itemReportTimeSmall = document.createElement("small")
+        itemReportTimeSmall.textContent = time
+        itemWrapperDiv.appendChild(itemReportTimeSmall)
+
+        const itemReportDescriptionP = document.createElement("p")
+        itemReportDescriptionP.classList.add("mb-1")
+        itemReportDescriptionP.textContent = description
+        itemAnchor.appendChild(itemReportDescriptionP)
+
+        const itemReportFooterSmall = document.createElement("small")
+        itemReportFooterSmall.classList.add("fst-italic")
+        itemAnchor.appendChild(itemReportFooterSmall)
+
+        const itemReportFooterSmallEm = document.createElement("em")
+        itemReportFooterSmallEm.textContent = footer
+        itemReportFooterSmall.appendChild(itemReportFooterSmallEm)
+
+        return itemAnchor
+    }
+
+    return {
+        create
+    }
+}
+
+function ReportsList(reportsDataSelector) {
+    const listGroup = document.querySelector(reportsDataSelector)
+    const reportListItem = ReportsListItem()
+    const defaultFooter = listGroup.dataset.footer
+    const notFound=  listGroup.querySelector(".not-found").cloneNode(true)
+
+    function clean() {
+        listGroup.innerHTML = ""
+        hideNotFound()
+        listGroup.appendChild(notFound)
+    }
+
+    function push({name, description, time, footer}) {
+        const item = reportListItem.create(name, description, time, footer ?? defaultFooter)
+        listGroup.appendChild(item)
+    }
+
+    function showNotFound() {
+        notFound.removeAttribute("hidden")
+    }
+
+    function hideNotFound() {
+        notFound.setAttribute("hidden", "")
+    }
+
+    function get() {
+        const items = listGroup.querySelectorAll("a")
+        return [].reduce.call(items, (reports, report) => {
+            return [...reports, {
+                time: report.querySelector("div > small").textContent,
+                name: report.querySelector("h1").textContent,
+                description: report.querySelector("p").textContent
+            }]
+        }, [])
+    }
+
+    return {
+        get,
+        clean,
+        push,
+        showNotFound,
+        hideNotFound
+    }
+}
+
+function ReportsFilter(reportsFilterSelector, reportsDataSelector) {
+    const filterWrapper = document.querySelector(reportsFilterSelector)
+    const input = filterWrapper.querySelector("input")
+    const resetButton = filterWrapper.querySelector("button")
+    const reportsList = ReportsList(reportsDataSelector)
+    const reportsListData = reportsList.get()
+
+    function getFilteredData(value) {
+        value = value.toLowerCase().removeAccents()
+
+        return reportsListData.filter(report => {
+            let match = false
+
+            Object.keys(report).forEach(key => {
+                if(report[key].toLowerCase().removeAccents().indexOf(value) !== -1) {
+                    match = true
+                    return
+                }
+            })
+
+            return match
+        })
+    }
+
+    function show(reports) {
+        reportsList.clean()
+
+        if(reports.length === 0) {
+            reportsList.showNotFound()
+            return
+        }
+
+        reportsList.hideNotFound()
+
+        reports.forEach(reportsList.push)
+    }
+
+    function clean() {
+        input.value = ""
+
+        reportsList.clean()
+        reportsListData.forEach(reportsList.push)
+    }
+
+    function listen() {
+        input.addEventListener("input", () => {
+            const filteredData = getFilteredData(input.value)
+            show(filteredData)
+        })
+
+        resetButton.addEventListener("click", clean)
+    }
+
+    return {
+        listen
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const internalReports = ReportsFilter("#internal-reports-filter", "#internal-reports-list")
+    const saveReportsFilter = ReportsFilter("#save-reports-filter", "#save-reports-list")
+
+    internalReports.listen()
+    saveReportsFilter.listen()
+})
--- src/App/views/pages/reports/index.php
+++ src/App/views/pages/reports/index.php
@@ -12,14 +12,22 @@
             <div class="p-3 col-md-12 col-lg-6 p-lg-5 pt-lg-3 reports-lg-border-end">
                 <h1 class="display-5 mb-3 text-center">Relatórios internos</h1>
 
-                <div class="reports list-group shadow">
+                <div id="internal-reports-filter" class="d-flex mb-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Digite o nome do relatório" aria-label="Pesquisar">
+                    <button class="btn btn-outline-secondary" type="button">Limpar</button>
+                </div>
+
+                <div id="internal-reports-list" class="reports list-group shadow" data-footer="Relatório gerado pela equipe do cpd">
+                    <div class="list-group-item list-group-item-action flex-column align-items-start not-found" hidden>
+                        <p class="mb-1 text-center py-3">Relatório não encontrado</p>
+                    </div>
                     <?php foreach($internalReports as $report): ?>
                         <a href="<?= $report["url"] ?>" class="list-group-item list-group-item-action flex-column align-items-start">
                             <div class="d-flex w-100 justify-content-between">
                                 <h1 class="h5 mb-1"><?= ucfirst($report["name"]); ?></h1>
                                 <small><?= $report["time"] ?? "" ?></small>
                             </div>
-                            <p class="mb-1"><?= $report["description"]; ?></p>
+                            <p class="mb-1"><?= ucfirst(mb_strtolower($report["description"])); ?></p>
                             <small class="fst-italic"><em>Relatório gerado pela equipe do cpd</em></small>
                         </a>
                     <?php endforeach; ?>
@@ -29,7 +37,15 @@
             <div class="p-3 col-md-12 col-lg-6 p-lg-5 pt-lg-3">
                 <h1 class="display-5 mb-3 text-center">Relatórios do S.A.V.E</h1>
 
-                <div class="reports list-group shadow">
+                <div id="save-reports-filter" class="d-flex mb-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Digite o nome do relatório" aria-label="Pesquisar">
+                    <button class="btn btn-outline-secondary" type="button">Limpar</button>
+                </div>
+
+                <div id="save-reports-list" class="reports list-group shadow" data-footer="Relatório gerado durante a rotina batch">
+                    <div class="list-group-item list-group-item-action flex-column align-items-start not-found" hidden>
+                        <p class="mb-1 text-center py-3">Relatório não encontrado</p>
+                    </div>
                     <?php foreach($saveReports as $report): ?>
                         <a href="" class="list-group-item list-group-item-action flex-column align-items-start">
                             <div class="d-flex w-100 justify-content-between">
@@ -45,3 +61,7 @@
         </div>
     </div>
 </main>
+
+<?php $this->setSection("footer"); ?>
+    <script src="<?= Helpers::baseUrl("/assets/js/reports/scripts.js") ?>"></script>
+<?php $this->endSection("footer"); ?>
