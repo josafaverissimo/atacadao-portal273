ae914e06ec6643abbd64e94c9f7d4b532e5440d7
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-05 12:53:04 -0300

    some changes

--- src/Core/config.php
+++ src/Core/config.php
@@ -1,15 +1,12 @@
 <?php
 
-const CONF_BASE_URL = "https://phplab.josafavgomes.repl.co";
-
+const CONF_BASE_URL = "https://atacadao-portal273";
 const CONF_BASE_VIEW_PATH = __DIR__ . "/../App/views";
-
 const CONF_DATASET_PATH = __DIR__ . "/../dataset";
-
 const CONF_DEFAULT_CONTROLLER = "Home:index";
-
 const CONF_CONTROLLER_BUILDING_PAGE = "BuildingPage:index";
-
 const CONF_CONTROLLER_ERROR_404 = "Error:error404";
-
 const CONF_PAGE_DEFAULT_ICON = "/assets/imgs/page-icon.ico";
+const CONF_DB_DSN = "mysql:dbname=internal_system;host=127.0.0.1";
+const CONF_DB_USER = "josafaverissimo";
+const CONF_DB_PASSWORD = "root";
