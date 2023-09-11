c97fad365e9ff739a129a16af8e439c8a2e27118
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-11 09:55:39 -0300

    feat(Home): load units phones by model

--- src/App/Controllers/Home.php
+++ src/App/Controllers/Home.php
@@ -3,23 +3,20 @@
 namespace Src\App\Controllers;
 
 use Src\Core\Controller;
-use Src\App\Dataset\UnitsPhones;
 use Src\App\Models\BirthdayPeopleModel;
 use Src\App\Models\UnitsPhonesModel;
-use Src\App\Models\Orms\UnitPhoneOrm;
 
 class Home extends Controller
 {
     public function index(): void
     {
         $birthdayPeopleModel = new BirthdayPeopleModel();
-        $unitsPhonesModel = UnitsPhonesModel();
+        $unitsPhonesModel = new UnitsPhonesModel();
 
-        $unitsPhonesDataset = new UnitsPhones();
         $data = [
             "title" => "Portal Interno Filial 273",
             "birthdayPeople" => $birthdayPeopleModel->getAll(),
-            "unitPhones" => $unitsPhonesDataset->getUnitPhonesByUnitId(273)
+            "unitPhones" => $unitsPhonesModel->getBy("id", 257)
         ];
 
         $this->renderView("/pages/home/index", $data);
