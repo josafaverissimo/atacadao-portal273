f32bcaf782651cc775fc4c41465380fad1160a83
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-13 05:19:08 -0300

    feat(pages/crudx): expand tables cards

--- src/App/views/pages/crudx/index.php
+++ src/App/views/pages/crudx/index.php
@@ -8,61 +8,72 @@ use Src\Utils\Helpers; ?>
 <?php $this->endSection("head"); ?>
 
 
-<main class="container-fluid container-lg">
-    <div class="p-4 pb-0 p-lg-0 pt-lg-5 p-md-5 align-items-center rounded-3 border shadow-lg mb-3 bg-white">
-        <div class="col-12 p-3 p-lg-5 pt-lg-3">
-            <h1 class="display-3 fst-italic">CrudX</h1>
-            <p class="lead my-3">
-                Cadastre, Filtre, Atualize ou Delete.
-            </p>
+<main>
+    <div class="container-fluid container-lg">
+        <div class="p-4 pb-0 p-lg-0 pt-lg-5 p-md-5 align-items-center rounded-3 border shadow-lg mb-3 bg-white">
+            <div class="col-12 p-3 p-lg-5 pt-lg-3">
+                <h1 class="display-3 fst-italic">CrudX</h1>
+                <p class="lead my-3">
+                    Cadastre, Filtre, Atualize ou Delete.
+                </p>
+            </div>
         </div>
     </div>
 
-    <div class="row">
-        <?php foreach($tables as $table): ?>
-            <div class="col-lg-3 col-lg-4 pb-3 mb-3 update-table">
-                <div class="d-flex flex-column align-items-center pt-2">
-                    <div class="card shadow" style="width: 100%">
-                        <div class="card-header">
-                            <h1 class="h5 m-0 text-center"><?= $table["name"] ?></h1>
-                        </div>
-
-                        <div class="p-2 d-flex flex-column align-items-center">
-                            <div class="mb-1 table-actions">
-                                <button
-                                    class="btn btn-outline-dark text-sm update-button"
-                                    type="button"
-                                    data-target="<?= Helpers::baseUrl("/crudx/table/{$table["tableToUpdate"]}") ?>"
-                                >
-                                    Atualizar
-                                </button>
-                                <button class="btn btn-outline-dark text-sm mx-2" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"/>
-                                    </svg>
-                                </button>
+    <div class="container-fluid container-lg" style="min-width: 90vw">
+        <div class="row">
+            <?php foreach($tables as $table): ?>
+                <div class="col-lg-3 col-lg-4 pb-3 mb-3 update-table">
+                    <div class="d-flex flex-column align-items-center pt-2">
+                        <div class="card shadow" style="width: 100%">
+                            <div class="card-header">
+                                <h1 class="h5 m-0 text-center"><?= $table["name"] ?></h1>
                             </div>
-                            <div id="<?= $table["tableToUpdate"] ?>-search-filter" class="d-flex" role="search">
-                                <input class="form-control me-2 text-sm" type="search" placeholder="Digite algo para pesquisar" aria-label="Pesquisar">
-                                <button class="btn btn-outline-secondary text-sm" type="button">Limpar</button>
-                            </div>
-                        </div>
 
-                        <?=
-                            $this->getViewHtml("/components/my-table", [
-                                "classes" => "my-table",
-                                "thead" => $table["columns"],
-                                "rows" => $table["rows"],
-                                "attributes" => [
-                                    "data-search-filter" => "#{$table["tableToUpdate"]}-search-filter",
-                                    "data-table-to-update" => "{$table["tableToUpdate"]}"
-                                ],
-                            ]);
-                        ?>
+                            <div class="p-2 d-flex flex-column align-items-center">
+                                <div class="mb-1 table-actions">
+                                    <button class="btn btn-outline-dark text-sm btn-sm">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                                        </svg>
+                                    </button>
+                                    <button
+                                        class="btn btn-outline-dark text-sm update-button mx-2 btn-sm"
+                                        data-target="<?= Helpers::baseUrl("/crudx/table/{$table["tableToUpdate"]}") ?>"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                                        </svg>
+                                    </button>
+                                    <button class="btn btn-outline-dark text-sm btn-sm" type="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-angle-expand" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div id="<?= $table["tableToUpdate"] ?>-search-filter" class="d-flex" role="search">
+                                    <input class="form-control me-2 text-sm" type="search" placeholder="Digite algo para pesquisar" aria-label="Pesquisar">
+                                    <button class="btn btn-outline-secondary text-sm" type="button">Limpar</button>
+                                </div>
+                            </div>
+
+                            <?=
+                                $this->getViewHtml("/components/my-table", [
+                                    "classes" => "my-table",
+                                    "thead" => $table["columns"],
+                                    "rows" => $table["rows"],
+                                    "attributes" => [
+                                        "data-search-filter" => "#{$table["tableToUpdate"]}-search-filter",
+                                        "data-table-to-update" => "{$table["tableToUpdate"]}"
+                                    ],
+                                ]);
+                            ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
 </main>
 
