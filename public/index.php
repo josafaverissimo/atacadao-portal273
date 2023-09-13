092720b26ed70c22560333851b91242700abb8f8
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-13 05:18:28 -0300

    add(index.php): login routes added

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -6,23 +7,28 @@ use Src\Core\Router\Router;
 
 $router = new Router();
 
-$router->get("/", CONF_DEFAULT_CONTROLLER);
+$router->get("/", CONF_DEFAULT_CONTROLLER)->middlewares("Auth");
 $router->get("/link-center", "LinkCenter:index");
 $router->get("/clock", "Clock:index");
 $router->get("/best-practices", CONF_CONTROLLER_BUILDING_PAGE);
 
-$router->group(["prefix" => "reports"], function() {
+$router->group(["prefix" => "login"], function () {
+    $this->get("/", "Login:index");
+    $this->get("/do-login", "Login:doLogin");
+});
+
+$router->group(["prefix" => "reports"], function () {
     $this->get("/", "Reports:index");
     $this->get("/printers", "Reports:printers");
     $this->get("/printers/getPrinterData/(:any)", "Reports:getPrinterData");
 });
 
-$router->group(["prefix" => "phones"], function() {
+$router->group(["prefix" => "phones"], function () {
     $this->get("/", "Phones:index");
     $this->get("/unitPhones/unitId/(:any)", "Phones:getUnitPhonesByUnitId");
 });
 
-$router->group(["prefix" => "crudx"], function() {
+$router->group(["prefix" => "crudx"], function () {
     $this->get("/", "Crudx:index");
     $this->get("/table/(:alpha)", "Crudx:updateTable");
 });
