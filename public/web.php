e513b6818ddf9b2f453673d46b6c592e6787b454
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-16 02:27:16 -0300

    feat(web.php): middleware Auth added in reports routes

--- public/web.php
+++ public/web.php
@@ -4,7 +4,7 @@ use Src\Core\Router\Router;
 
 $router = new Router();
 
-$router->get("/", CONF_DEFAULT_CONTROLLER)->middlewares("Auth");
+$router->get("/", CONF_DEFAULT_CONTROLLER);
 $router->get("/link-center", "LinkCenter:index");
 $router->get("/clock", "Clock:index");
 $router->get("/best-practices", CONF_CONTROLLER_BUILDING_PAGE);
@@ -14,7 +14,7 @@ $router->group(["prefix" => "login"], function () {
     $this->get("/do-login", "Login:doLogin");
 });
 
-$router->group(["prefix" => "reports"], function () {
+$router->group(["prefix" => "reports", "middlewares" => ["Auth"]], function () {
     $this->get("/", "Reports:index");
     $this->get("/printers", "Reports:printers");
     $this->get("/printers/getPrinterData/(:any)", "Reports:getPrinterData");
