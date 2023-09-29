cfd3eccfaf269ab1d16d1f88d5351fecbdf90f38
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-09-29 16:31:48 -0300

    add(config.php): constants create to sockets metadatas

--- src/Core/config.php
+++ src/Core/config.php
@@ -13,3 +13,13 @@ const CONF_PAGE_DEFAULT_ICON = "/assets/imgs/page-icon.ico";
 const CONF_DB_DSN = "mysql:dbname=internal_system;host=127.0.0.1";
 const CONF_DB_USER = "josafaverissimo";
 const CONF_DB_PASSWORD = "root";
+
+const CONF_BIRTHDAY_PEOPLE_SOCKET_HOST = "localhost";
+const CONF_BIRTHDAY_PEOPLE_SOCKET_PORT = 8080;
+const CONF_BIRTHDAY_PEOPLE_SOCKET_HTTP_METHOD = "GET";
+const CONF_BIRTHDAY_PEOPLE_SOCKET_PATH = "/json/birthday-people.json";
+
+const CONF_UNITS_SOCKET_HOST = "localhost";
+const CONF_UNITS_SOCKET_PORT = 8080;
+const CONF_UNITS_SOCKET_HTTP_METHOD = "GET";
+const CONF_UNITS_SOCKET_PATH = "/json/units.json";
